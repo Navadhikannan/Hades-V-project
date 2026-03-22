0435daf32090a659b1649633aaee48261eb0de85
Navadhikannan <navadhi2306k@gmail.com>
2026-03-22 17:47:23 +0100

    Exercise 2: Implement Fetch Stage

--- rtl/fetch_stage.sv
+++ rtl/fetch_stage.sv
@@ -1,30 +1,72 @@
-/* Copyright (c) 2024 Tobias Scheipel, David Beikircher, Florian Riedl
- * Embedded Architectures & Systems Group, Graz University of Technology
- * SPDX-License-Identifier: MIT
- * ---------------------------------------------------------------------
- * File: fetch_stage.sv
- */
-
-
-
 module fetch_stage (
     input logic clk,
     input logic rst,
-
-    // Memory interface
     wishbone_interface.master wb,
-
-    //  Output data
     output logic [31:0] instruction_reg_out,
     output logic [31:0] program_counter_reg_out,
-
-    // Pipeline control
     output pipeline_status::forwards_t  status_forwards_out,
     input  pipeline_status::backwards_t status_backwards_in,
     input  logic [31:0] jump_address_backwards_in
 );
 
-    // TODO: Delete the following line and implement this module.
-    ref_fetch_stage golden(.*);
+    logic [31:0] pc;
+
+    // Next PC combinatorial logic
+    logic [31:0] pc_next;
+    always_comb begin
+        if (status_backwards_in == pipeline_status::JUMP)
+            pc_next = jump_address_backwards_in;
+        else if (status_backwards_in == pipeline_status::STALL)
+            pc_next = pc;
+        else
+            pc_next = pc + 32'd4;
+    end
+
+    // Wishbone - present current PC as address
+    assign wb.cyc      = 1'b1;
+    assign wb.stb      = 1'b1;
+    assign wb.adr      = pc >> 2;
+    assign wb.we       = 1'b0;
+    assign wb.sel      = 4'b1111;
+    assign wb.dat_mosi = 32'b0;
+
+    // PC and output registers
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            pc                      <= constants::RESET_ADDRESS;
+            instruction_reg_out     <= constants::NOP;
+            program_counter_reg_out <= constants::RESET_ADDRESS;
+            status_forwards_out     <= pipeline_status::BUBBLE;
+        end
+        else begin
+            pc <= pc_next;
+
+            if (status_backwards_in == pipeline_status::STALL) begin
+                instruction_reg_out     <= instruction_reg_out;
+                program_counter_reg_out <= program_counter_reg_out;
+                status_forwards_out     <= status_forwards_out;
+            end
+            else if (status_backwards_in == pipeline_status::JUMP) begin
+                instruction_reg_out     <= constants::NOP;
+                program_counter_reg_out <= pc;
+                status_forwards_out     <= pipeline_status::BUBBLE;
+            end
+            else if (wb.err) begin
+                instruction_reg_out     <= constants::NOP;
+                program_counter_reg_out <= pc;
+                status_forwards_out     <= pipeline_status::FETCH_FAULT;
+            end
+            else if (wb.ack) begin
+                instruction_reg_out     <= wb.dat_miso;
+                program_counter_reg_out <= pc;
+                status_forwards_out     <= pipeline_status::VALID;
+            end
+            else begin
+                instruction_reg_out     <= constants::NOP;
+                program_counter_reg_out <= pc;
+                status_forwards_out     <= pipeline_status::BUBBLE;
+            end
+        end
+    end
 
 endmodule
