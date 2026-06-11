ad67de7c7a5eba8f42c31de45c99b93d237f9d1f
Navadhikannan <navadhi2306k@gmail.com>
2026-06-11 16:30:49 +0200

    Fix instruction decoder: FENCE immediate and CSR validation

--- rtl/instruction_decoder.sv
+++ rtl/instruction_decoder.sv
@@ -200,20 +200,40 @@ module instruction_decoder (
             32'b????????????_?????_001_?????_1110011: begin
                 instruction_out.op          = op::CSRRW;
                 instruction_out.rs2_address = 5'b0;
+                case (instruction_in[31:20])
+                    12'h300, 12'h304, 12'h305, 12'h340, 12'h341,
+                    12'h342, 12'h344, 12'hB00, 12'hB02, 12'hB80, 12'hB82: ;
+                    default: instruction_out.op = op::ILLEGAL;
+                endcase
             end
             32'b????????????_?????_010_?????_1110011: begin
                 instruction_out.op          = op::CSRRS;
                 instruction_out.rs2_address = 5'b0;
+                case (instruction_in[31:20])
+                    12'h300, 12'h304, 12'h305, 12'h340, 12'h341,
+                    12'h342, 12'h344, 12'hB00, 12'hB02, 12'hB80, 12'hB82: ;
+                    default: instruction_out.op = op::ILLEGAL;
+                endcase
             end
             32'b????????????_?????_011_?????_1110011: begin
                 instruction_out.op          = op::CSRRC;
                 instruction_out.rs2_address = 5'b0;
+                case (instruction_in[31:20])
+                    12'h300, 12'h304, 12'h305, 12'h340, 12'h341,
+                    12'h342, 12'h344, 12'hB00, 12'hB02, 12'hB80, 12'hB82: ;
+                    default: instruction_out.op = op::ILLEGAL;
+                endcase
             end
             32'b????????????_?????_101_?????_1110011: begin
                 instruction_out.op          = op::CSRRWI;
                 instruction_out.rs1_address = 5'b0;
                 instruction_out.rs2_address = 5'b0;
                 instruction_out.immediate   = {27'b0, instruction_in[19:15]};
+                case (instruction_in[31:20])
+                    12'h300, 12'h304, 12'h305, 12'h340, 12'h341,
+                    12'h342, 12'h344, 12'hB00, 12'hB02, 12'hB80, 12'hB82: ;
+                    default: instruction_out.op = op::ILLEGAL;
+                endcase
             end
             32'b????????????_?????_110_?????_1110011: begin
                 instruction_out.op          = op::CSRRSI;
